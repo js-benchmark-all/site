cc781a498e169fd3d9ff32f33317c8fe8cdd147e
aquapi <aquaplmc@gmail.com>
2025-08-10 21:30:32 +0700

    Update

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,11 +4,16 @@ let startupSize: Record<
   string,
   Record<string, Record<string, [string, number][]>>
 >;
-const getStartupSize = async () => startupSize = await (
-  await fetch("https://raw.githubusercontent.com/js-benchmark-all/startup-size/refs/heads/main/.results/index.json"
-)).json();
-
-setTimeout(getStartupSize, 60 * 1000);
+const unsetStartupSize = () => {
+  startupSize = null as any;
+}
+const getStartupSize = async () => {
+  startupSize = await (
+    await fetch("https://raw.githubusercontent.com/js-benchmark-all/startup-size/refs/heads/main/.results/index.json"
+  )).json();
+  setTimeout(unsetStartupSize, 60000);
+  return startupSize;
+}
 
 export const server = {
   getStartupSize: defineAction({
