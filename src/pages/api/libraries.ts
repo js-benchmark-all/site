eb615c7f05f064b282e3cc7bf4d5357b738b7ba6
aquapi <aquaplmc@gmail.com>
2026-02-14 17:25:24 +0700

    fix race

--- src/pages/api/libraries.ts
+++ src/pages/api/libraries.ts
@@ -4,13 +4,27 @@ let result: Response | null = null;
 const clearResult = () => {
   result = null;
 };
-const loadResult = async (): Promise<Response> => {
-  const res = await fetch("https://raw.githubusercontent.com/js-benchmark-all/startup-size/refs/heads/dev/result.json");
-  result = new Response(await res.text());
-  setTimeout(clearResult, 10000);
-  return result.clone();
+const cloneResult = () => result!.clone();
+
+let loading: Promise<void> | null = null;
+const loadResult = async () => {
+  try {
+    let totalTime = performance.now();
+
+    const res = await fetch("https://raw.githubusercontent.com/js-benchmark-all/startup-size/refs/heads/dev/result.json");
+    result = new Response(await res.text());
+    setTimeout(clearResult, 10000);
+
+    totalTime = performance.now() - totalTime;
+    console.info('Loading new results:', totalTime);
+  } catch (e) {
+    console.error(e);
+    result = new Response('Failed to fetch!');
+  }
+
+  loading = null;
 }
 
 export const GET: APIRoute = () => result === null
-  ? loadResult()
-  : result.clone();
+  ? (loading === null ? loading = loadResult() : loading).then(cloneResult)
+  : cloneResult();
